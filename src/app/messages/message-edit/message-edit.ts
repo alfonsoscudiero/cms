2fb54225242a6b0137e56f38d1d1fbe8cdda4c73
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-05-09 16:42:29 -0600

    Create message components and message model

---
+++ src/app/messages/message-edit/message-edit.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cms-message-edit',
+  standalone: false,
+  templateUrl: './message-edit.html',
+  styleUrl: './message-edit.css',
+})
+export class MessageEdit {}
