08eab2413a0aec2bb2019dbb090e60b5051523ac
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-05-09 17:07:24 -0600

    Implement component view switching with EventEmitter

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,16 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'cms-root',
-  templateUrl: './app.html',
   standalone: false,
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('cms');
+
+  selectedFeature = 'documents';
+
+  switchView(selectedFeature: string) {
+    this.selectedFeature = selectedFeature;
+  }
 }
