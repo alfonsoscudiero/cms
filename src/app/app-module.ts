8587781440f7e2227ea103856e1c21786bf8067b
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-05-09 16:26:18 -0600

    Create document components and initial layout

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,9 +7,24 @@ import { Contacts } from './contacts/contacts';
 import { ContactList } from './contacts/contact-list/contact-list';
 import { ContactDetail } from './contacts/contact-detail/contact-detail';
 import { ContactItem } from './contacts/contact-item/contact-item';
+import { Documents } from './documents/documents';
+import { DocumentList } from './documents/document-list/document-list';
+import { DocumentItem } from './documents/document-item/document-item';
+import { DocumentDetail } from './documents/document-detail/document-detail';
 
 @NgModule({
-  declarations: [App, Header, Contacts, ContactList, ContactDetail, ContactItem],
+  declarations: [
+    App,
+    Header,
+    Contacts,
+    ContactList,
+    ContactDetail,
+    ContactItem,
+    Documents,
+    DocumentList,
+    DocumentItem,
+    DocumentDetail,
+  ],
   imports: [BrowserModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
