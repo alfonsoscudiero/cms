def57686bf3429d3b9851b17e68f4ed33038ffcd
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-05-16 10:39:01 -0600

    Implement DocumentDetailComponent bindings

--- src/app/documents/document-detail/document-detail.ts
+++ src/app/documents/document-detail/document-detail.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Document } from '../document.model';
 @Component({
   selector: 'cms-document-detail',
   standalone: false,
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.css',
 })
-export class DocumentDetail {}
+export class DocumentDetail {
+  @Input() document!: Document;
+}
