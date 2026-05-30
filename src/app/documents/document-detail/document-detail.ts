3996d6fdb68a625f4d2bd8ad22da614e43ed0ede
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-05-29 19:54:59 -0600

    Implement document detail route parameter handling

--- src/app/documents/document-detail/document-detail.ts
+++ src/app/documents/document-detail/document-detail.ts
@@ -1,11 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 @Component({
   selector: 'cms-document-detail',
   standalone: false,
   templateUrl: './document-detail.component.html',
   styleUrl: './document-detail.component.css',
 })
-export class DocumentDetail {
-  @Input() document!: Document;
+export class DocumentDetail implements OnInit {
+  document!: Document;
+
+  constructor(
+    private documentService: DocumentService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id'];
+      const document = this.documentService.getDocument(id);
+
+      if (document) {
+        this.document = document;
+      }
+    });
+  }
+
 }
