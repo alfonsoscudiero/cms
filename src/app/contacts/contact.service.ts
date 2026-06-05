7e900a38cca5682cc44e5fe4c07e98fcaf3809ae
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-05 16:29:09 -0600

    Add updateContact method to ContactService

--- src/app/contacts/contact.service.ts
+++ src/app/contacts/contact.service.ts
@@ -60,6 +60,26 @@ export class ContactService {
     this.contactListChangedEvent.next(contactsListClone);
   }
 
+  updateContact(originalContact: Contact, newContact: Contact) {
+    if (!originalContact || !newContact) {
+      return;
+    }
+
+    const pos = this.contacts.indexOf(originalContact);
+
+    if (pos < 0) {
+      return;
+    }
+
+    newContact.id = originalContact.id;
+
+    this.contacts[pos] = newContact;
+
+    const contactsListClone = this.contacts.slice();
+
+    this.contactListChangedEvent.next(contactsListClone);
+  }
+
   deleteContact(contact: Contact) {
     if (!contact) {
       return;
