c725c609c036c84f57e5da4c396cced3b39c32b1
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-12 17:26:56 -0600

    Add ngOnInit, onSubmit, and onCancel methods to ContactEdit component

--- src/app/contacts/contact-edit/contact-edit.ts
+++ src/app/contacts/contact-edit/contact-edit.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 @Component({
   selector: 'cms-contact-edit',
   standalone: false,
@@ -10,18 +11,52 @@ import { Contact } from '../contact.model';
   styleUrl: './contact-edit.css',
 })
 export class ContactEdit {
+  originalContact: Contact | null = null;
   contact: Contact | null = null;
   groupContacts: Contact[] = [];
+  editMode: boolean = false;
+  id!: string;
 
   constructor(
-      private router: Router,
-    ) {}
+    private contactService: ContactService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+
+        if (this.id == null) {
+          this.editMode = false;
+          return;
+        }
+
+        this.originalContact = this.contactService.getContact(this.id);
+
+        if (this.originalContact == null) {
+          return;
+        }
+
+        this.editMode = true;
+
+        this.contact = JSON.parse(
+          JSON.stringify(this.originalContact)
+        );
+
+        if (this.originalContact.group) {
+          this.groupContacts = JSON.parse(
+            JSON.stringify(this.originalContact.group)
+          );
+        }
+      }
+    );
+  }
 
   onSubmit(form: NgForm) {
     console.log(form.value);
-  }
+  }  
 
   onCancel() {
     this.router.navigate(['/contacts']);
