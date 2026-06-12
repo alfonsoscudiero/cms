0c879316ea3b100fa415d5d36b9a3dc0f91b2088
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-12 17:10:54 -0600

    Add contact edit field binding and validation

--- src/app/contacts/contact-edit/contact-edit.ts
+++ src/app/contacts/contact-edit/contact-edit.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
+import { Contact } from '../contact.model';
 @Component({
   selector: 'cms-contact-edit',
   standalone: false,
@@ -8,10 +10,20 @@ import { NgForm } from '@angular/forms';
   styleUrl: './contact-edit.css',
 })
 export class ContactEdit {
+  contact: Contact | null = null;
+  groupContacts: Contact[] = [];
+
+  constructor(
+      private router: Router,
+    ) {}
 
   ngOnInit(): void {}
 
   onSubmit(form: NgForm) {
     console.log(form.value);
+  }
+
+  onCancel() {
+    this.router.navigate(['/contacts']);
   }
 }
