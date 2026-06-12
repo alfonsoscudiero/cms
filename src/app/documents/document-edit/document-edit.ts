954f389ee5dd8bf55201979824922d5f57293615
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-12 16:14:58 -0600

    Add Angular form binding, validation, and submit handling to document edit form

--- src/app/documents/document-edit/document-edit.ts
+++ src/app/documents/document-edit/document-edit.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-edit',
@@ -6,4 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './document-edit.html',
   styleUrl: './document-edit.css',
 })
-export class DocumentEdit {}
+
+export class DocumentEdit implements OnInit {
+  originalDocument!: Document;
+  document!: Document;
+  editMode: boolean = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+  }
+
+  onCancel() {
+    this.router.navigate(['/documents']);
+  }
+}
