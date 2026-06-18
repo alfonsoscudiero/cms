32d192482b2184a3de00c1f8112e97640466bbab
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-06-18 10:48:27 -0600

    Implement contact filtering logic in ContactsFilterPipe

--- src/app/contacts-filter-pipe.ts
+++ src/app/contacts-filter-pipe.ts
@@ -1,11 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+import { Contact } from './contacts/contact.model';
+
 @Pipe({
   name: 'contactsFilter',
   standalone: false,
 })
+
 export class ContactsFilterPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(contacts: Contact[], term: string): Contact[] {
+    let filteredContacts: Contact[] = [];
+
+    if (term && term.length > 0) {
+      filteredContacts = contacts.filter(
+        (contact: Contact) =>
+          contact.name.toLowerCase().includes(term.toLowerCase())
+      );
+    }
+
+    if (filteredContacts.length < 1) {
+      return contacts;
+    }
+
+    return filteredContacts;
   }
 }
