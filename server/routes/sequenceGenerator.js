c5fed422e95dde9ce6f6ae2e5e60ebfc4fd9578a
alfonsoscudiero <alfonsoscudiero@gmail.com>
2026-07-02 19:58:11 -0600

    Update sequence generator to use modern Mongoose promises

--- server/routes/sequenceGenerator.js
+++ server/routes/sequenceGenerator.js
@@ -1,62 +1,59 @@
-var Sequence = require('../models/sequence');
+const Sequence = require('../models/sequence');
 
-var maxDocumentId;
-var maxMessageId;
-var maxContactId;
-var sequenceId = null;
+let maxDocumentId;
+let maxMessageId;
+let maxContactId;
+let sequenceId = null;
 
 function SequenceGenerator() {
-
   Sequence.findOne()
-    .exec(function(err, sequence) {
-      if (err) {
-        return res.status(500).json({
-          title: 'An error occurred',
-          error: err
-        });
+    .then(sequence => {
+      if (!sequence) {
+        console.log('SequenceGenerator error: No sequence document found.');
+        return;
       }
 
       sequenceId = sequence._id;
       maxDocumentId = sequence.maxDocumentId;
       maxMessageId = sequence.maxMessageId;
       maxContactId = sequence.maxContactId;
+    })
+    .catch(err => {
+      console.log('SequenceGenerator error = ' + err);
     });
 }
 
 SequenceGenerator.prototype.nextId = function(collectionType) {
-
-  var updateObject = {};
-  var nextId;
+  let updateObject = {};
+  let nextId;
 
   switch (collectionType) {
     case 'documents':
       maxDocumentId++;
-      updateObject = {maxDocumentId: maxDocumentId};
+      updateObject = { maxDocumentId: maxDocumentId };
       nextId = maxDocumentId;
       break;
     case 'messages':
       maxMessageId++;
-      updateObject = {maxMessageId: maxMessageId};
+      updateObject = { maxMessageId: maxMessageId };
       nextId = maxMessageId;
       break;
     case 'contacts':
       maxContactId++;
-      updateObject = {maxContactId: maxContactId};
+      updateObject = { maxContactId: maxContactId };
       nextId = maxContactId;
       break;
     default:
       return -1;
   }
 
-  Sequence.update({_id: sequenceId}, {$set: updateObject},
-    function(err) {
-      if (err) {
-        console.log("nextId error = " + err);
-        return null
-      }
+  Sequence.updateOne({ _id: sequenceId }, { $set: updateObject })
+    .catch(err => {
+      console.log('nextId error = ' + err);
+      return null;
     });
 
   return nextId;
-}
+};
 
 module.exports = new SequenceGenerator();
